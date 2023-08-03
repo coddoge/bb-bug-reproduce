5ad2aacc904966f2faf69de6b593ab07cc630ec5
Coder <coder@example.com>
2023-08-03 18:45:15 +0800

    2 bugs about `Collapse` of `Card`

---
+++ CardBugs1/Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddBootstrapBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+await builder.Build().RunAsync();
